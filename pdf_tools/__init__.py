15fbb5989fb54e11c183439b5475acc8c307b0e1
CoalGravel <cole.gravelle@gmail.com>
2020-11-10 01:23:48 -0500

    fixed package layout and importation

---
+++ pdf_tools/__init__.py
@@ -0,0 +1,4 @@
+__all__ =  ["combine_pdf", "compress_img", "compress_pdf", "del_page_pdf",
+            "img_from_doc", "img_from_pdf", "img_from_ppt", "img_from_xls",
+            "pdf_from_doc", "pdf_from_img", "pdf_from_ppt", "pdf_from_xls",
+            "reorder_pdf", "split_pdf"]
